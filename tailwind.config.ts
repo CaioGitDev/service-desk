7e2383728b175a50035572bc0d871df6e9cdf6ac
Caio Rosa <dev.caiorosa@gmail.com>
2024-06-25 11:55:36 +0100

    chore: Update npm dependencies to include react-icons

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,13 +13,6 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
-      colors: {
-        'primary-color': 'var(--primary-gold)',
-        'secondary-color': 'var(--secondary)',
-        'tertiary-color': 'var(--tertiary)',
-        'quaternary-color': 'var(--quaternary)',
-        'primary-text-light': 'var(--primary-text-light)',
-      },
     },
   },
   plugins: [],
